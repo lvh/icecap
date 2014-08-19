0cfb325711f9063e9d848cd30374209315e3a94d
Laurens Van Houtven <_@lvh.io>
2014-08-19 17:45:05 +0200

    Use s/conditional instead of s/either
    This gives better error reporting.
    
    Closes #1.

--- src/icecap/schema.clj
+++ src/icecap/schema.clj
@@ -21,7 +21,25 @@
                    (s/pred supported-scheme? "supported-scheme?"))})
 
 (def RequestSpec
-  (s/either
-   SimpleRequest
-   [(s/recursive #'RequestSpec)]
-   #{(s/recursive #'RequestSpec)}))
+  "A request specification.
+
+  Please note that this uses prismatic/schema's `conditional` with
+  type dispatch, rather than the (perhaps more obvious) `either`. The
+  problem with `either` is that it didn't know which of the schemas
+  *probably* should have validated: if you give it a bogus
+  `SimpleRequest`, it can only tell you that it:
+
+  - wasn't a valid `SimpleRequest`
+  - wasn't an unordered collection of valid `SimpleRequests`
+  - wasn't an ordered collection of valid `SimpleRequests`
+
+  ... but it fails to tell you that it *was* something that looked
+  like a SimpleRequest but didn't validate because, for example, its
+  target was `ftp`, which is an unsupported URL scheme. That would've
+  been far more useful. Using `conditional` allows us to return
+  this (useful) error message.
+  "
+  (s/conditional
+   map? SimpleRequest
+   vector? [(s/recursive #'RequestSpec)]
+   set? #{(s/recursive #'RequestSpec)}))
