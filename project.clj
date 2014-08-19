0126817e9ceb8117e03b408c29d56c74a05684ea
Laurens Van Houtven <_@lvh.io>
2014-08-19 16:34:08 +0200

    Add coordinates to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject icecap "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A secure capability URL system"
+  :url "https://github.com/lvh/icecap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
