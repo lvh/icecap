c68b061a2ce9c068363aa609a50c4698ec5d4b2b
Laurens Van Houtven <_@lvh.io>
2014-09-02 11:10:38 +0200

    Add components

--- src/icecap/core.clj
+++ src/icecap/core.clj
@@ -1,12 +1,13 @@
 (ns icecap.core
   (:require [icecap.rest :as rest]
+            [icecap.store.mem :refer [mem-store]]
             [org.httpkit.server :refer [run-server]])
   (:gen-class))
 
 (defn -main
   "Run the (development) server."
   [& args]
-  (let [handler (if (= args ["dev"])
-               rest/reloading-site
-               rest/site)]
+  (let [dev-mode (= args ["dev"])
+        components {:store (mem-store)}
+        handler (rest/build-site components dev-mode)]
     (run-server handler {:port 8080})))
--- src/icecap/rest.clj
+++ src/icecap/rest.clj
@@ -1,7 +1,7 @@
 (ns icecap.rest
   "The REST API for icecap."
   (:require [compojure.core :refer [defroutes context GET POST DELETE]]
-            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults secure-api-defaults]]
             [ring.middleware.format :refer [wrap-restful-format]]
             [ring.middleware.reload :refer [wrap-reload]]))
 
@@ -12,13 +12,30 @@
                     (GET "/" [] "GET")
                     (DELETE "/" [] "DELETE"))))
 
-(def site
-  "The site."
-  (-> routes
-      ;; TODO: api-defaults -> secure-defaults
-      (wrap-defaults api-defaults)
-      (wrap-restful-format :formats [:edn])))
+(defn ^:private wrap-components
+  "Adds some components to each request map."
+  [handler components]
+  (fn [request]
+    (handler (merge request components))))
 
-(def reloading-site
-  "Like the regular site, except automatically reloading."
-  (wrap-reload site))
+(defn build-site
+  "Builds a site.
+
+  All requests will have the given components available. Incoming data
+  EDN data will be parsed. Outgoing data structures will be serialized
+  as EDN.
+
+  The site's behavior will have some sane defaults. If it is running
+  in dev mode, secure defaults (which include automatically
+  redirecting to HTTPS) are disabled. Additionally, the server will
+  automatically reload code.
+  "
+  [components & {dev-mode :dev-mode :or {dev-mode false}}]
+  (let [defaults (if dev-mode api-defaults secure-api-defaults)
+        site (-> routes
+                 (wrap-defaults defaults)
+                 (wrap-restful-format :formats [:edn])
+                 (wrap-components components))]
+    (if dev-mode
+      (wrap-reload site)
+      site)))
