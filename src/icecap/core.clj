d7661faeaae1c44b59bcb81acd42092c5dc7f3dc
Laurens Van Houtven <_@lvh.io>
2014-09-02 18:26:34 +0200

    More bogus dev components

--- src/icecap/core.clj
+++ src/icecap/core.clj
@@ -1,5 +1,6 @@
 (ns icecap.core
   (:require [icecap.rest :as rest]
+            [icecap.crypto :refer [bogus-kdf bogus-scheme]]
             [icecap.store.mem :refer [mem-store]]
             [org.httpkit.server :refer [run-server]])
   (:gen-class))
@@ -8,6 +9,6 @@
   "Run the (development) server."
   [& args]
   (let [dev-mode (= args ["dev"])
-        components {:store (mem-store)}
-        handler (rest/build-site components dev-mode)]
+        components {:store (mem-store) :kdf (bogus-kdf) :scheme (bogus-scheme)}
+        handler (rest/build-site components :dev-mode dev-mode)]
     (run-server handler {:port 8080})))
