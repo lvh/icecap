789ada48594d86e91c3794d31a22c6a7f15f8b2f
Laurens Van Houtven <_@lvh.io>
2014-09-03 16:28:44 +0200

    More docs about handlers

--- src/icecap/handlers/core.clj
+++ src/icecap/handlers/core.clj
@@ -8,6 +8,11 @@
 (defmacro defstep
   "Define a step implementation.
 
+  A step implementation is defined by its schema (a map), and a
+  handler (a sequence of forms). The handler forms will have the step
+  being handled injected into it, and should evaluate to a core.async
+  chan.
+
   The schema does not have to include the step's `:type`; it is
   implicitly added.
   "
