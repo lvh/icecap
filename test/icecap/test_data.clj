9fd29d165259ef24e798dfbb87c36d1d2e87dc38
Laurens Van Houtven <_@lvh.io>
2014-09-05 12:02:29 +0200

    Parametrize success-step

--- test/icecap/test_data.clj
+++ test/icecap/test_data.clj
@@ -1,6 +1,6 @@
 (ns icecap.test-data)
 
-(def simple-step {:type :succeed})
+(defn success-step [name] {:type :succeed :name name})
 (def simple-http-step {:type :http :url "http://example.test"})
 (def simple-https-step {:type :http :url "https://example.test"})
 (def simple-ftp-step {:type :ftp :loc "ftp://example.test"})
