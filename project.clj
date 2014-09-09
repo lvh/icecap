384dbfda585d862f7863a71753f947aeb6e7e3c8
Laurens Van Houtven <_@lvh.io>
2014-09-09 15:45:37 +0200

    Use Clojure 1.7 alpha to get transducers

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/lvh/icecap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
 
                  ;; Logging
                  [com.taoensso/timbre "3.3.0" :exclusions [org.clojure/clojure]]
