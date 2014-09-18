0a927b1d530e90a1a462b7e388d3e03956fe7d2c
Laurens Van Houtven <_@lvh.io>
2014-09-18 14:57:05 +0200

    Version bumps

--- project.clj
+++ project.clj
@@ -6,15 +6,15 @@
   :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
 
                  ;; Logging
-                 [com.taoensso/timbre "3.3.0" :exclusions [org.clojure/clojure]]
+                 [com.taoensso/timbre "3.3.1" :exclusions [org.clojure/clojure]]
 
                  ;; Handlers
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
-                 [http-kit "2.1.16"]
+                 [http-kit "2.1.19"]
 
                  ;; Schemata
                  [prismatic/schema "0.2.6"]
-                 [schema-contrib "0.1.3"]
+                 [schema-contrib "0.1.5"]
                  [cddr/integrity "0.2.0-20140823.193326-1" :exclusions [org.clojure/clojure]]
 
                  ;; Crypto
@@ -29,12 +29,12 @@
 
                  ;; REST API
                  ;; http-kit already required as part of handlers
-                 [compojure "1.1.8"]
+                 [compojure "1.1.9"]
                  [ring/ring-defaults "0.1.1"]
                  [ring-middleware-format "0.4.0"]]
   :main ^:skip-aot icecap.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[prone "0.4.0"]
+             :dev {:dependencies [[prone "0.6.0"]
                                   [peridot "0.3.0"]
                                   [org.clojure/test.check "0.5.9"]]}})
