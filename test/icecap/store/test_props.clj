e98919684dc5b4c4ddbf6f4a4f5d35ad32af6708
Laurens Van Houtven <_@lvh.io>
2014-09-22 16:45:31 +0200

    Add a store-delete prop

--- test/icecap/store/test_props.clj
+++ test/icecap/store/test_props.clj
@@ -1,6 +1,6 @@
 (ns icecap.store.test-props
   (:require [caesium.crypto.util :refer [array-eq]]
-            [icecap.store.api :refer [create! retrieve]]
+            [icecap.store.api :refer [create! delete! retrieve]]
             [icecap.crypto :refer [index-bytes]]
             [icecap.test-props :refer [n-bytes]]
             [clojure.core.async :refer [<!!]]
@@ -14,3 +14,11 @@
                 (do (<!! (create! store index blob))
                     (array-eq (<!! (retrieve store index))
                               blob))))
+
+(defn store-delete-prop
+  [store]
+  (prop/for-all [index (n-bytes index-bytes)
+                 blob gen/bytes]
+                (do (<!! (create! store index blob))
+                    (<!! (delete! store index))
+                    (nil? (<!! (retrieve store index))))))
