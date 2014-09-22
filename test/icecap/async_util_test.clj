75858c1bca3d9ef6270506fbba206327a06d3abf
Laurens Van Houtven <_@lvh.io>
2014-09-22 16:12:31 +0200

    Add more tests

--- test/icecap/async_util_test.clj
+++ test/icecap/async_util_test.clj
@@ -1,19 +1,24 @@
-(ns icecap.async-util
+(ns icecap.async-util-test
   (:require [clojure.core.async :refer [<!! go] :as a]
             [icecap.async-util :refer :all]
             [clojure.test :refer :all]))
 
+(defn ^:private assert-false?
+  "Checks if this is an exception matching the one raised when
+  evaluating `(assert false)`."
+  [e]
+  (and (instance? AssertionError e)
+       (= (.getMessage e) "Assert failed: false")))
+
 (defmacro ^:private has-exc
-  "Checks if the given chan has a failed assertion on it, and then closes."
-  [c]
-  `(is
-    (let [e# (<!! ~c)
-          n# (<!! ~c)]
-      (prn e#)
-      (prn n#)
-      (and (instance? AssertionError e#)
-           (= (.getMessage e#)
-              "Assert failed: false")           ))))
+  "Checks if the given chan has a failed assertion on it, and then
+  closes."
+  [chan-expr]
+  `(is (let [c# ~chan-expr
+             exception# (<!! c#)
+             next-val# (<!! c#)]
+         (and (assert-false? exception#)
+              (nil? next-val#)))))
 
 (deftest go-catch-tests
   (testing "returns exceptions"
@@ -26,8 +31,19 @@
 
 (deftest <!!?-tests
   (testing "take a value"
-    (is (= (<!!? (go 1))
-           1)))
+    (is (= (<!!? (go 1 2 3))
+           3)))
   (testing "raise an exc"
     (is (thrown-with-msg? AssertionError #"Assert failed: false"
                           (<!!? (go-catch (assert false)))))))
+
+(deftest <!?-tests
+  (testing "take a value"
+    (let [inner-chan (a/to-chan [1 2 3])
+          outer-chan (go (<!? inner-chan))]
+      (is (= (<!! outer-chan)
+             1))))
+  (testing "raise an exc"
+    (let [err-chan (go-catch (assert false))
+          <!?-chan (go-catch (<!? err-chan))]
+      (is (assert-false? (<!! <!?-chan))))))
