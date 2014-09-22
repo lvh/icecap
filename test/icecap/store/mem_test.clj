42ccefb60484acaf0df0b1601f1788e9f838091e
Laurens Van Houtven <_@lvh.io>
2014-09-22 17:00:00 +0200

    Add roundtrip test to mem-store

--- test/icecap/store/mem_test.clj
+++ test/icecap/store/mem_test.clj
@@ -2,10 +2,13 @@
   (:require [caesium.crypto.util :refer [array-eq]]
             [icecap.store.api :refer :all]
             [icecap.store.mem :refer :all]
-            [icecap.store.test-props :refer [store-roundtrip-prop]]
+            [icecap.store.test-props :refer :all]
             [clojure.test :refer :all]
             [clojure.core.async :as a :refer [<!!]]
             [clojure.test.check.clojure-test :refer [defspec]]))
 
 (defspec mem-store-roundtrip
   (store-roundtrip-prop (mem-store)))
+
+(defspec mem-store-delete
+  (store-delete-prop (mem-store)))
