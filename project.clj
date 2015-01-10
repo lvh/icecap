901348045a98bf789fa6b73a2d18a1948e53100c
Laurens Van Houtven <_@lvh.io>
2015-01-09 17:21:48 -0800

    Upgrade prismatic/schema

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
                  [http-kit "2.1.19"]
 
                  ;; Schemata
-                 [prismatic/schema "0.3.0"]
+                 [prismatic/schema "0.3.3"]
                  [schema-contrib "0.1.5"]
                  [cddr/integrity "0.2.0-20140823.193326-1" :exclusions [org.clojure/clojure]]
 
