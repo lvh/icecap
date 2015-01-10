8eb972aa0d5737b9495aa1a3c5c9c526799f2956
Laurens Van Houtven <_@lvh.io>
2015-01-09 17:23:39 -0800

    Upgrade compojure

--- project.clj
+++ project.clj
@@ -28,7 +28,7 @@
 
                  ;; REST API
                  ;; http-kit already required as part of handlers
-                 [compojure "1.2.0"]
+                 [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
                  [ring-middleware-format "0.4.0"]
                  [prone "0.6.0"]
