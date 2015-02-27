7634432379a0e61726eb48bc12ac9a37a4e8a9ac
Laurens Van Houtven <_@lvh.io>
2015-02-26 23:22:21 -0600

    Cleanup

--- test/icecap/test_data.clj
+++ test/icecap/test_data.clj
@@ -1,6 +1,15 @@
 (ns icecap.test-data)
 
-(defn success-step [name] {:type :succeed :name name})
-(def simple-http-step {:type :http :url "http://example.test"})
-(def simple-https-step {:type :http :url "https://example.test"})
-(def simple-ftp-step {:type :ftp :loc "ftp://example.test"})
+(defn success-step
+  [name]
+  {:type :succeed :name name})
+
+(def simple-http-step
+  {:type :http :url "http://example.test"})
+
+(def simple-https-step
+  {:type :http :url "https://example.test"})
+
+(def simple-ftp-step
+  {:type :ftp
+   :loc "ftp://example.test"})
