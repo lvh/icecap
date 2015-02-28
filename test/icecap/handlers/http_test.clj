5e8458f502372778ad45a8b2ed2e5c4df3a34478
Laurens Van Houtven <_@lvh.io>
2015-02-28 15:04:28 -0600

    Functional wankery
    Cc @glyph @cyli

--- test/icecap/handlers/http_test.clj
+++ test/icecap/handlers/http_test.clj
@@ -18,10 +18,9 @@
     (are [s] (nil? (check-plan s))
          step))
   (testing "invalid steps"
-    (are [s expected] (= (let [step (merge {:type :http} s)
-                               result (check-plan step)]
-                           (pr-str result))
-                         (pr-str expected))
+    (are [s expected] (let [step (merge {:type :http} s)
+                            reprs (map pr-str [(check-plan step) expected])]
+                        (apply = reprs))
          {} '{:url missing-required-key}
          {:uri url} '{:uri disallowed-key
                       :url missing-required-key}
