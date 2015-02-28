e7cbb02788d9903e5b4398b805af0d4c6f57e902
Laurens Van Houtven <_@lvh.io>
2015-02-28 14:45:00 -0600

    Schema test
    Refs #14.

--- test/icecap/handlers/http_test.clj
+++ test/icecap/handlers/http_test.clj
@@ -2,12 +2,18 @@
   (:require [clojure.core.async :as a]
             [manifold.deferred :as d]
             [icecap.handlers.core :refer [execute]]
+            [icecap.schema :refer [check-plan]]
             [icecap.handlers.http]
             [aleph.http :as h]
             [clojure.test :refer :all]))
 
 (def step
   {:type :http :url "http://www.example.com"})
+
+(deftest schema-tests
+  (testing "valid steps"
+    (are [x] (nil? (check-plan x))
+         step)))
 
 (def fake-response
   {})
