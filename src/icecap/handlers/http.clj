c21548aef72115376252dc37f929f93d3aa3a94c
Laurens Van Houtven <_@lvh.io>
2015-04-14 10:39:55 -0400

    Cleanup HTTP

--- src/icecap/handlers/http.clj
+++ src/icecap/handlers/http.clj
@@ -1,14 +1,15 @@
 (ns icecap.handlers.http
   (:require [aleph.http :refer [request]]
-            [clojure.core.async :refer [chan]]
+            [clojure.core.async :refer [to-chan]]
             [icecap.handlers.core :refer [defstep]]
             [manifold.stream :refer [connect]]
-            [schema-contrib.core :refer [URI]]))
+            [schema-contrib.core :refer [URI]]
+            [taoensso.timbre :refer [info spy]]))
 
 (defstep :http
   {:url URI}
   [step]
-  (let [ch (chan)
-        req (request step)]
-    (connect req ch)
-    ch))
+  (let [args (spy (select-keys step [:url]))
+        req (spy (request args))
+        res (spy @req)]
+    (to-chan [res])))
