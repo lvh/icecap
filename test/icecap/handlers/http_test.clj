e509c75628289629939f9103b13136524bb29191
Laurens Van Houtven <_@lvh.io>
2015-04-16 13:34:09 -0400

    Use more test data

--- test/icecap/handlers/http_test.clj
+++ test/icecap/handlers/http_test.clj
@@ -4,25 +4,18 @@
             [icecap.handlers.core :refer [execute]]
             [icecap.schema :refer [check-plan]]
             [icecap.handlers.http]
+            [icecap.test-data :refer [simple-http-step simple-https-step]]
             [aleph.http :as h]
             [clojure.test :refer :all]))
-
-(def url
-  "http://www.example.com")
-
-(def step
-  {:type :http
-   :url url
-   :method :GET})
 
 (deftest schema-tests
   (testing "valid steps"
     (are [s] (nil? (check-plan s))
-         (merge step {:method :GET})
-         (merge step {:method :HEAD})
-         (merge step {:method :POST})
-         (merge step {:method :DELETE})
-         (merge step {:method :PUT})))
+         simple-http-step
+         (merge simple-http-step {:method :HEAD})
+         (merge simple-http-step {:method :POST})
+         (merge simple-http-step {:method :DELETE})
+         (merge simple-http-step {:method :PUT})))
   (testing "invalid steps"
     (are [s expected]  (= (check-plan (merge {:type :http} s))
                           expected)
@@ -31,7 +24,7 @@
          '{:url missing-required-key
            :method missing-required-key}
 
-         {:uri url}
+         {:uri (:url simple-http-step)}
          '{:uri disallowed-key
            :url missing-required-key
            :method missing-required-key}
@@ -40,7 +33,7 @@
          '{:url (throws? (URI 1))
            :method missing-required-key}
 
-         {:url url
+         {:url (:url simple-http-step)
           :method :BOGUS}
          '{:method (not (#{:DELETE
                            :HEAD
