a0defb495246f9d5fd461a975b021841da2334b7
Laurens Van Houtven <_@lvh.io>
2015-04-17 17:21:44 -0400

    Reformat

--- test/icecap/handlers/http_test.clj
+++ test/icecap/handlers/http_test.clj
@@ -11,56 +11,56 @@
 (deftest schema-tests
   (testing "valid steps"
     (are [s] (nil? (check-plan s))
-         simple-http-step
-         (merge simple-http-step {:method :HEAD})
-         (merge simple-http-step {:method :POST})
-         (merge simple-http-step {:method :DELETE})
-         (merge simple-http-step {:method :PUT})
-         simple-https-step
-         (merge simple-https-step {:method :HEAD})
-         (merge simple-https-step {:method :POST})
-         (merge simple-https-step {:method :DELETE})
-         (merge simple-https-step {:method :PUT})))
+      simple-http-step
+      (merge simple-http-step {:method :HEAD})
+      (merge simple-http-step {:method :POST})
+      (merge simple-http-step {:method :DELETE})
+      (merge simple-http-step {:method :PUT})
+      simple-https-step
+      (merge simple-https-step {:method :HEAD})
+      (merge simple-https-step {:method :POST})
+      (merge simple-https-step {:method :DELETE})
+      (merge simple-https-step {:method :PUT})))
   (testing "invalid steps"
     (are [s expected]  (= (check-plan (merge {:type :http} s))
                           expected)
 
-         {}
-         '{:url missing-required-key
-           :method missing-required-key}
+      {}
+      '{:url missing-required-key
+        :method missing-required-key}
 
-         {:uri (:url simple-http-step)}
-         '{:uri disallowed-key
-           :url missing-required-key
-           :method missing-required-key}
+      {:uri (:url simple-http-step)}
+      '{:uri disallowed-key
+        :url missing-required-key
+        :method missing-required-key}
 
-         {:url 1}
-         '{:url (throws? (URI 1))
-           :method missing-required-key}
+      {:url 1}
+      '{:url (throws? (URI 1))
+        :method missing-required-key}
 
-         {:url (:url simple-http-step)
-          :method :BOGUS}
-         '{:method (not (#{:DELETE
-                           :HEAD
-                           :GET
-                           :PATCH
-                           :POST
-                           :PUT}
-                         :BOGUS))}
+      {:url (:url simple-http-step)
+       :method :BOGUS}
+      '{:method (not (#{:DELETE
+                        :HEAD
+                        :GET
+                        :PATCH
+                        :POST
+                        :PUT}
+                      :BOGUS))}
 
-         {:url "bogus://example.test"
-          :method :GET}
-         '{:url (not (valid-scheme? a-java.lang.String))})))
+      {:url "bogus://example.test"
+       :method :GET}
+      '{:url (not (valid-scheme? a-java.lang.String))})))
 
 (deftest valid-scheme?-tests
   (testing "valid schemes"
     (are [url] (valid-scheme? url)
-         "http://example.test"
-         "https://example.test"))
+      "http://example.test"
+      "https://example.test"))
   (testing "invalid schemes"
     (are [url] (not (valid-scheme? url))
-         "bogus://example.test"
-         "ftp://example.test")))
+      "bogus://example.test"
+      "ftp://example.test")))
 
 (def fake-response
   ::my-fake-response)
@@ -71,7 +71,7 @@
 
 (deftest execute-tests
   (with-redefs
-    [h/request fake-request]
+   [h/request fake-request]
     (is (let [ch (execute simple-http-step)
               [result] (a/<!! (a/into [] ch))]
           (= result fake-response)))))
