d713d919fe3472be10b14544367e677da07b3f3e
Hatnik <support@hatnik.com>
2015-04-29 05:01:41 +0000

    com.taoensso/nippy 2.9.0-RC1 released

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [com.novemberain/welle "3.0.0"]
 
                  ;; Serialization
-                 [com.taoensso/nippy "2.9.0-beta2"
+                 [com.taoensso/nippy "2.9.0-RC1"
                   :exclusions [org.clojure/clojure com.taoensso/encore]]
 
                  ;; Handlers
