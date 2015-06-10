ca9d3e7029345dac992e8b0901a653afc068a3aa
Hatnik <support@hatnik.com>
2015-06-10 07:01:36 +0000

    com.taoensso/timbre 4.0.0 released

--- project.clj
+++ project.clj
@@ -28,7 +28,7 @@
                  [caesium "0.3.0"]
 
                  ;; Logging
-                 [com.taoensso/timbre "4.0.0-RC1"
+                 [com.taoensso/timbre "4.0.0"
                   :exclusions [org.clojure/clojure]]
 
                  ;; REST API
