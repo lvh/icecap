7d4f5c260d4503a5679f354b5406c468fa0e959f
Hatnik <support@hatnik.com>
2015-06-26 06:02:22 +0000

    org.clojure/core.typed 0.3.2 released

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
                   :exclusions [instaparse]]
                  [cddr/integrity "0.3.0-SNAPSHOT"
                   :exclusions [org.clojure/clojure]]
-                 [org.clojure/core.typed "0.3.1"]
+                 [org.clojure/core.typed "0.3.2"]
 
                  ;; Crypto
                  [caesium "0.3.0"]
