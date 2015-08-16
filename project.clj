169323d9f10ca8f3d644dad9c806da9e2552ef3e
Laurens Van Houtven <_@lvh.io>
2015-08-16 16:15:10 -0700

    Update kibit

--- project.clj
+++ project.clj
@@ -58,4 +58,4 @@
                               :exclusions [rewrite-clj]]
                              [lein-bikeshed "0.1.8"]
                              [lein-cljfmt "0.1.10"]
-                             [lein-kibit "0.1.0"]]}})
+                             [lein-kibit "0.1.2"]]}})
