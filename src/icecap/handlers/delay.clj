4416dba6b466eb96c02b20d745828afa6b5b2df9
Laurens Van Houtven <_@lvh.io>
2015-09-10 20:43:06 -0700

    Fix schema import

--- src/icecap/handlers/delay.clj
+++ src/icecap/handlers/delay.clj
@@ -1,7 +1,7 @@
 (ns icecap.handlers.delay
   (:require [clojure.core.async :refer [timeout]]
             [icecap.handlers.core :refer [defstep]]
-            [prismatic.schema.core :as sc]))
+            [schema.core :as sc]))
 
 (defstep :delay
   {:amount (sc/both sc/Int (sc/pred #(< 0 % 60)'(< 0 delay 60)))}
