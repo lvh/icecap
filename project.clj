faf9c43746c91a660d77debbe7590f40ad1b5604
Hatnik <support@hatnik.com>
2015-10-06 11:02:25 +0000

    com.taoensso/nippy 2.11.0-alpha5 released

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [com.novemberain/welle "3.0.0"]
 
                  ;; Serialization
-                 [com.taoensso/nippy "2.11.0-alpha3"
+                 [com.taoensso/nippy "2.11.0-alpha5"
                   :exclusions [org.clojure/clojure com.taoensso/encore]]
 
                  ;; Handlers
