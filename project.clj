34eb042e8e66a8e73da38b3113dd517949bb5b94
Hatnik <support@hatnik.com>
2015-11-02 13:01:59 +0000

    lein-ancient 0.6.8 released

--- project.clj
+++ project.clj
@@ -53,7 +53,7 @@
                                         ["bikeshed"]
                                         ["eastwood"]]}
                    :plugins [[jonase/eastwood "0.1.4"]
-                             [lein-ancient "0.6.7"
+                             [lein-ancient "0.6.8"
                               :exclusions [rewrite-clj]]
                              [lein-bikeshed "0.1.8"]
                              [lein-cljfmt "0.3.0"]
