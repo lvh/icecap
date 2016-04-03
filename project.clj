07e5ec14e85379c894159d2b404e50c1e159474f
Laurens Van Houtven <_@lvh.io>
2016-04-03 11:00:48 -0500

    Upgrade prismatic/schema

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
                  [aleph "0.4.1-beta3"]
 
                  ;; Schemata
-                 [prismatic/schema "1.0.4"]
+                 [prismatic/schema "1.1.0"]
                  [schema-contrib "0.1.5"
                   :exclusions [instaparse]]
                  [org.clojure/core.typed "0.3.19"]
