c0d2aa4cf4598a64971beb2c6dd6007deb1f5a84
Laurens Van Houtven <_@lvh.io>
2016-04-03 19:51:55 -0500

    Update aleph

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
 
                  ;; Handlers
                  [org.clojure/core.async "0.2.374"]
-                 [aleph "0.4.1-beta3"]
+                 [aleph "0.4.1"]
 
                  ;; Schemata
                  [prismatic/schema "1.1.0"]
