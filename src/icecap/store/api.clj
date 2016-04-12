20bd9572f1fcbf0fe3ba612600337af38f17a21b
Laurens Van Houtven <_@lvh.io>
2016-04-12 17:37:09 -0500

    Update store API docs

--- src/icecap/store/api.clj
+++ src/icecap/store/api.clj
@@ -6,15 +6,13 @@
   (create! [store index blob]
     "Stores `blob` at `index`.
 
-    Returns a core.async chan that will close when the blob has been
-    stored.")
+    Returns a deferred that will fire when the blob has been stored.")
   (retrieve [store index]
     "Get the blob at `index`.
 
-     Returns a core.async chan that returns the stored blob, then
-     closes. If the blob does not exist, just closes the channel.")
+     Returns a deferred that fires with the stored blob or `nil`.")
   (delete! [store index]
     "Delete the blob at `index`.
 
-    Returns a core.async chan that will close when the blob has been
-    deleted, even if there wasn't a blob at `index`."))
+    Returns a deferred that fires when the blob has been deleted, even if
+    there wasn't a blob at `index`."))
